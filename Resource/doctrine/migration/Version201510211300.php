3adcd5038a49d2e8e724d6c7a278181f80462835
lqdung <lqdung@lockon-vn.com>
2016-10-13 16:59:55 +0700

    [ADD] Add migration for old version <= 3.0.8

--- Resource/doctrine/migration/Version201510211300.php
+++ Resource/doctrine/migration/Version201510211300.php
@@ -14,6 +14,7 @@ use Doctrine\DBAL\Migrations\AbstractMigration;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\ORM\Tools\SchemaTool;
 use Eccube\Application;
+use Eccube\Common\Constant;
 
 /**
  * Class Version201510211300
@@ -26,6 +27,10 @@ class Version201510211300 extends AbstractMigration
      */
     const NAME = 'plg_recommend_product';
 
+    protected $entities = array(
+        'Plugin\Recommend\Entity\RecommendProduct',
+    );
+
     /**
      * Setup data
      *
@@ -33,7 +38,11 @@ class Version201510211300 extends AbstractMigration
      */
     public function up(Schema $schema)
     {
-        $this->createRecommendProduct($schema);
+        if (version_compare(Constant::VERSION, '3.0.9', '>=')) {
+            $this->createRecommendProduct($schema);
+        } else {
+            $this->createRecommendProductForOldVersion($schema);
+        }
     }
 
     /**
@@ -43,9 +52,28 @@ class Version201510211300 extends AbstractMigration
      */
     public function down(Schema $schema)
     {
-        // this down() migration is auto-generated, please modify it to your needs
-        $schema->dropTable(self::NAME);
-        $schema->dropSequence('plg_recommend_product_recommend_product_id_seq');
+        if (version_compare(Constant::VERSION, '3.0.9', '>=')) {
+            $app = Application::getInstance();
+            $meta = $this->getMetadata($app['orm.em']);
+            $tool = new SchemaTool($app['orm.em']);
+            $schemaFromMetadata = $tool->getSchemaFromMetadata($meta);
+            // テーブル削除
+            foreach ($schemaFromMetadata->getTables() as $table) {
+                if ($schema->hasTable($table->getName())) {
+                    $schema->dropTable($table->getName());
+                }
+            }
+            // シーケンス削除
+            foreach ($schemaFromMetadata->getSequences() as $sequence) {
+                if ($schema->hasSequence($sequence->getName())) {
+                    $schema->dropSequence($sequence->getName());
+                }
+            }
+        } else {
+            // this down() migration is auto-generated, please modify it to your needs
+            $schema->dropTable(self::NAME);
+            $schema->dropSequence('plg_recommend_product_recommend_product_id_seq');
+        }
     }
 
     /**
@@ -69,5 +97,69 @@ class Version201510211300 extends AbstractMigration
         $tool->createSchema($classes);
 
         return true;
+    }
+
+
+    /**
+     * おすすめ商品テーブル作成
+     *
+     * @param Schema $schema
+     */
+    protected function createRecommendProductForOldVersion(Schema $schema)
+    {
+        $table = $schema->createTable(self::NAME);
+        $table->addColumn('recommend_product_id', 'integer', array(
+            'autoincrement' => true,
+            'notnull' => true,
+        ));
+
+        $table->addColumn('product_id', 'integer', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->addColumn('comment', 'text', array(
+            'notnull' => false,
+        ));
+
+        $table->addColumn('rank', 'integer', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 1,
+        ));
+
+        $table->addColumn('del_flg', 'smallint', array(
+            'notnull' => true,
+            'unsigned' => false,
+            'default' => 0,
+        ));
+
+        $table->addColumn('create_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->addColumn('update_date', 'datetime', array(
+            'notnull' => true,
+            'unsigned' => false,
+        ));
+
+        $table->setPrimaryKey(array('recommend_product_id'));
+    }
+
+    /**
+     * Get metadata
+     *
+     * @param EntityManager $em
+     * @return array
+     */
+    protected function getMetadata(EntityManager $em)
+    {
+        $meta = array();
+        foreach ($this->entities as $entity) {
+            $meta[] = $em->getMetadataFactory()->getMetadataFor($entity);
+        }
+
+        return $meta;
     }
 }
